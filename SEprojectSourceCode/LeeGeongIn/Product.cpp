667d22de2ce2f9f246f75863276cc7684f8f7f23
LeeGeongIn <leeji9991@gmail.com>
2022-05-27 15:46:36 +0900

    sourcecode 주석 수정

--- SEprojectSourceCode/LeeGeongIn/Product.cpp
+++ SEprojectSourceCode/LeeGeongIn/Product.cpp
@@ -2,6 +2,12 @@
 #include <string>
 using namespace std;
 
+// Function: Product(string seller, string name, string company, string price, string quentity)
+// Description: Product Class의 매개변수 생성자
+// Parameter: string seller- 판매자 ID, string name - 물품명, string company - 제조회사명, string price - 물품가격, 
+//            string quentity - 남은수량
+// Return Value: None
+// Author: 이정인
 Product::Product(string seller, string name, string company, string price, string quentity)
 {
 	this->seller = seller;
--- SEprojectSourceCode/LeeGeongIn/ProductList.cpp
+++ SEprojectSourceCode/LeeGeongIn/ProductList.cpp
@@ -1,5 +1,11 @@
 #include "ProductList.h"
 
+// Function: createProduct(string seller, string name, string company, string price, string quentity)
+// Description: 새로운 Product를 생성해 ProductDB에 저장.
+// Parameter: string seller- 판매자 ID, string name - 물품명, string company - 제조회사명, string price - 물품가격, 
+//            string quentity - 남은수량
+// Return Value: void
+// Author: 이정인
 void ProductList::createProduct(string seller, string name, string company, string price, string quentity)
 {
 	Product* newProduct = new Product(seller, name, company, price, quentity);
