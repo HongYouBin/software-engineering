f3dee33092424e31f50bb39f1fa7ddea256f8ac6
LeeGeongIn <leeji9991@gmail.com>
2022-05-31 21:38:21 +0900

    통합용 program Code

---
+++ Program Code 통합본/SEproject/Product.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// Class : Product
+// Description: 물품 정보 저장 Class
+class Product
+{
+	string seller;          // 판매자 ID
+	string name;            // 상품명
+	string company;         // 상품 생산 회사명
+	string price;           // 상품 가격
+	string quentity;        // 수량
+	int evaluation = 0;     // 구매만족도
+public:
+	Product(string seller, string name, string company, string price, string quentity);
+};
+
