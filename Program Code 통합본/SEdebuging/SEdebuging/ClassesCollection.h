8b10c250362d9346655f60f22a2a0e0bfcdd616e
LeeGeongIn <leeji9991@gmail.com>
2022-06-02 03:39:34 +0900

    디버깅용 통합 코드

---
+++ Program Code 통합본/SEdebuging/SEdebuging/ClassesCollection.h
@@ -0,0 +1,273 @@
+#pragma once
+#include <vector>
+#include <string>
+using namespace std;
+
+class JoinMembership;
+class JoinMembershipUI;
+class Login;
+class LoginUI;
+class Logout;
+class LogoutUI;
+class ManageMember;
+class Member;
+class ProductList;
+class Product;
+class RegisterSellingClothing;
+class RegisterSellingClothingUI;
+class Withdrawal;
+class WithdrawalUI;
+class Search;
+class SearchUI;
+class PurchaseList;
+class PurchaseListUI;
+class Evaluation;
+class EvaluationUI;
+
+// Class : JoinMembership
+// Description: 회원가입 기능의 Control Class
+class JoinMembership
+{
+	JoinMembershipUI* joinMembershipUIPtr;     // JoinMembership 기능의 boundary class pointer
+	ManageMember* manageMemberPtr;             // systemMemberDB 객체의 주소를 저장하는 pointer
+public:
+	JoinMembership(ManageMember* manageMemberPtr);
+	JoinMembershipUI* getJoinMembershipUIPtr();
+	void addNewMember(string name, string SSN, string ID, string password);
+};
+
+// Class : JoinMembershipUI
+// Description: 회원가입 기능의 Boundary Class
+class JoinMembershipUI
+{
+	JoinMembership* joinMembershipPtr;      // JoinMembership 기능의 control class pointer
+public:
+	JoinMembershipUI(JoinMembership* joinMembershipPtr);
+	void startInterface();
+	void inputPersonalInfo(FILE* in_fp, FILE* out_fp);
+};
+
+// Class : Login
+// Description: 로그인 기능의 Control Class
+class Login
+{
+	LoginUI* loginUIPtr;                // Login 기능의 boundary class pointer
+	ManageMember* manageMemberPtr;      // systemMemberDB 객체의 주소를 저장하는 pointer
+public:
+	Login(ManageMember* manageMemberPtr);
+	LoginUI* getLoginUIPtr();
+	int checkMember(string ID, string password);
+
+};
+
+// Class : LoginUI
+// Description: 로그인 기능의 Boundary Class
+class LoginUI
+{
+	Login* loginPtr;           // Login 기능의 control class pointer
+public:
+	LoginUI(Login* loginPtr);
+
+	void InputIDPW(FILE* in_fp, FILE* out_fp);
+	void startInterface();
+};
+
+// Class : Logout
+// Description: 로그아웃 기능의 Control Class
+class Logout
+{
+	ManageMember* manageMemberPtr;       // systemMemberDB 객체의 주소를 저장하는 pointer
+public:
+	Logout(ManageMember* manageMemberPtr);
+	string logout();
+};
+
+// Class : LogoutUI
+// Description: 로그아웃 기능의 Boundary Class
+class LogoutUI
+{
+	Logout* logoutPtr;     // Logout 기능의 control class pointer
+public:
+	LogoutUI(Logout* logoutPtr, FILE* out_fp);
+	void selectLogout(FILE* out_fp);
+};
+
+
+// Class : ManageMember
+// Description: 시스템 내 회원 리스트 관리 및 로그인 회원 정보 관리, 회원 정보 확인을 위한 Class
+class ManageMember
+{
+	vector<Member> memberDB;          // 쇼핑몰 프로그램에 존재하는 전체 회원 저장 vector
+	vector<Member> loggedMembers;     // 현재 쇼핑몰 프로그램에 로그인한 회원 저장 vector
+public:
+	string getSellerID();
+	void createMember(string name, string SSN, string ID, string password);
+	int checkMember(string ID, string password);
+	string deleteMember();
+	string deleteLoggedMember();
+	void addPurchaseList(string productName);                   //
+	vector<string> getPurchaseList();                           //
+};
+
+// Class : Member
+// Description: 회원 정보 저장 Class
+class Member
+{
+	string name;         // 회원명
+	string ID;           // 회원 ID
+	string SSN;          // 주민번호
+	string password;     // 비밀번호
+	vector<string> purchaseList;         //
+public:
+	Member(string name, string SSN, string ID, string password);
+	string getID();
+	Member* registerLoggedMember();
+	vector<string> getPurchaseList();       //
+	void addPurchaseList(string productName); //
+};
+
+// Class : ProductList
+// Description: 시스템 내 물품 리스트 관리 Class
+class ProductList
+{
+	vector<Product> productDB;          // 쇼핑몰 프로그램에 존재하는 전체 물품 저장 vector
+public:
+	void createProduct(string seller, string name, string company, string price, string quentity);
+	Product* getProductInfo(string productName);       //
+	void purchase(Product* productName);                  //
+	Product* saveEvaluationInfo(string productName, int evaluation);    //
+};
+
+// Class : Product
+// Description: 물품 정보 저장 Class
+class Product
+{
+	string seller;          // 판매자 ID
+	string name;            // 상품명
+	string company;         // 상품 생산 회사명
+	string price;           // 상품 가격
+	int quentity;           // 수량
+	int evaluation = 0;     // 구매만족도
+	int numberOfEvaluation = 0;		//구매만족도에 참여한 인원 //
+public:
+	Product(string seller, string name, string company, string price, int quentity);
+	string getName();
+	string getSeller();
+	string getPrice();
+	string getCompany();
+	int getQuentity();
+	int getEvaluationAverage();
+	void purchase();
+	Product addEvaluation(int evaluation);
+	float getAverageEvaluation();
+};
+
+// Class : RegisterSellingClothing
+// Description: 판매 물품 등록 기능의 Control Class
+class RegisterSellingClothing
+{
+	RegisterSellingClothingUI* registerSellingClothingUIPtr;   // RegisterSellingClothing 기능의 boundary class pointer
+	ProductList* productListPtr;                               // systemProductDB 객체의 주소를 저장하는 pointer
+	ManageMember* manageMemberPtr;                             // systemMemberDB 객체의 주소를 저장하는 pointer
+public:
+	RegisterSellingClothing(ProductList* productListPtr, ManageMember* manageMemberPtr);
+	RegisterSellingClothingUI* getRegisterSellingClothingUIPtr();
+	void addNewProduct(string name, string company, string price, string quentity);
+};
+
+// Class : RegisterSellingClothingUI
+// Description: 판매 물품 등록 기능의 Boundary Class
+class RegisterSellingClothingUI
+{
+	RegisterSellingClothing* registerSellingClothingPtr;     // RegisterSellingClothing 기능의 control class pointer
+
+public:
+	RegisterSellingClothingUI(RegisterSellingClothing* registerSellingClothingPtr);
+	void startInterface();
+	void registerNewProduct(FILE* in_fp, FILE* out_fp);
+
+};
+
+// Class : Withdrawal
+// Description: 회원탈퇴 기능의 Control Class
+class Withdrawal
+{
+	WithdrawalUI* withdrawalUIPtr;       // Withdrawal 기능의 boundary class pointer
+	ManageMember* manageMemberPtr;       // systemMemberDB 객체의 주소를 저장하는 pointer
+public:
+	Withdrawal(ManageMember* manageMemberPtr);
+	WithdrawalUI* getWithdrawalUIPtr();
+	string cancelMembership();
+};
+
+// Class : WithdrawalUI
+// Description: 회원탈퇴 기능의 Boundary Class
+class WithdrawalUI
+{
+	Withdrawal* withdrawalPtr;       // Withdrawal 기능의 control class pointer
+public:
+	WithdrawalUI(Withdrawal* withdrawalPtr);
+	void startInterface();
+	void selectMembershipCancel(FILE* out_fp);
+};
+
+// -------------------------새로 추가된 .h들 ---------------
+
+class Search
+{
+private:
+	ProductList* productListPtr;
+	ManageMember* manageMemberPtr;
+public:
+	Search(ManageMember* manageMemberAddress, ProductList* productListAddress);
+	Product* searchProduct(string productName);
+	void purchaseProduct(Product* productPtr);
+};
+
+class SearchUI
+{
+private:
+	Search* searchPtr;
+	Product* selectedProduct;
+public:
+	SearchUI(ManageMember* manageMemberPtr, ProductList* productListPtr);
+	void searchProduct(FILE* productName, FILE* out_fp);
+	void purchase(FILE* out_fp);
+};
+
+class PurchaseList
+{
+private:
+	ProductList* productListPtr;
+	ManageMember* manageMemberPtr;
+public:
+	PurchaseList(ManageMember* manageMemberAddress, ProductList* productListAddress);
+	vector<Product> getPurchaseList();
+};
+
+class PurchaseListUI
+{
+private:
+	PurchaseList* purchaseListPtr;
+public:
+	PurchaseListUI(ManageMember* manageMemberAddress, ProductList* productListAddress);
+	void showProductList(FILE* out_fp);
+};
+
+class Evaluation
+{
+private:
+	ProductList* productListPtr;
+public:
+	Evaluation(ProductList* productList);
+	Product* saveEvaluation(string productName, int evaluation);
+};
+
+class EvaluationUI
+{
+private:
+	Evaluation* evaluationPtr;
+public:
+	EvaluationUI(ProductList* productListAddress);
+	void evaluateProduct(FILE* in_fp, FILE* out_fp);
+};
---
+++ Program Code 통합본/SEdebuging/SEdebuging/main.cpp
@@ -0,0 +1,138 @@
+#pragma once
+#include <stdio.h>
+#include "ClassesCollection.h"
+using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+// 상수 선언
+#define MAX_STRING 32
+#define INPUT_FILE_NAME "input.txt"
+#define OUTPUT_FILE_NAME "output.txt"
+// 함수 선언
+void doTask();
+void program_exit(FILE* out_fp);
+// 변수 선언
+FILE* in_fp, * out_fp;
+
+int main()
+{ 
+	doTask();
+	return 0;
+}
+
+void doTask()
+{
+	ProductList* systemProductDB = new ProductList;
+	ManageMember* systemMemberDB = new ManageMember;
+	SearchUI* searchUIPtr = new SearchUI(systemMemberDB, systemProductDB);                                                 // 추가
+	in_fp = fopen(INPUT_FILE_NAME, "r+"); out_fp = fopen(OUTPUT_FILE_NAME, "w+");
+	// 메뉴 파싱을 위한 level 구분을 위한 변수
+	int menu_level_1 = 0, menu_level_2 = 0; int is_program_exit = 0;
+	while (!is_program_exit)
+	{ // 입력파일에서 메뉴 숫자 2개를 읽기
+		fscanf(in_fp, "%d %d ", &menu_level_1, &menu_level_2);
+
+		// 메뉴 구분 및 해당 연산 수행
+		switch (menu_level_1)
+		{
+		case 1:
+		{
+			switch (menu_level_2)
+			{
+			case 1: // "1.1. 회원가입“ 메뉴 부분
+			{
+				JoinMembership* joinMembershipPtr = new JoinMembership(systemMemberDB);
+				joinMembershipPtr->getJoinMembershipUIPtr()->inputPersonalInfo(in_fp, out_fp);
+				break;
+			}
+			case 2: // "1.2. 회원탈퇴“ 메뉴 부분
+			{
+				Withdrawal* withdrawalPtr = new Withdrawal(systemMemberDB);
+				withdrawalPtr->getWithdrawalUIPtr()->selectMembershipCancel(out_fp);
+				break;
+			}
+			}
+			break;
+		}
+		case 2:
+		{
+			switch (menu_level_2)
+			{
+			case 1: // "2.1. 로그인“ 메뉴 부분
+			{
+				Login* loginPtr = new Login(systemMemberDB);
+				loginPtr->getLoginUIPtr()->InputIDPW(in_fp, out_fp);
+				break;
+			}
+			case 2: // "2.2. 로그아웃“ 메뉴 부분
+			{
+				Logout* logoutPtr = new Logout(systemMemberDB);
+				LogoutUI* logoutUIPtr = new LogoutUI(logoutPtr, out_fp);
+				break;
+			}
+			}
+			break;
+		}
+		case 3:
+		{
+			switch (menu_level_2)
+			{
+			case 1: // "3.1. 판매 의류 등록“ 메뉴 부분
+			{
+				RegisterSellingClothing* RegisterSellingClothingPtr = new RegisterSellingClothing(systemProductDB, systemMemberDB);
+				RegisterSellingClothingPtr->getRegisterSellingClothingUIPtr()->registerNewProduct(in_fp, out_fp);
+				break;
+			}
+			}
+			break;
+		}
+		case 4:
+		{
+			switch (menu_level_2)
+			{
+			case 1: // "4.1. “ 메뉴 부분
+			{
+				searchUIPtr->searchProduct(in_fp, out_fp);
+				break;
+			}
+			case 2: // "4.2. “ 메뉴 부분
+			{
+				searchUIPtr->purchase(out_fp);
+				break;
+			}
+			case 3: // "4.3. “ 메뉴 부분
+			{
+				PurchaseListUI* purchaseListUIPtr = new PurchaseListUI(systemMemberDB, systemProductDB);
+				purchaseListUIPtr->showProductList(out_fp);
+				break;
+			}
+			case 4: // "4.4. “ 메뉴 부분
+			{
+				EvaluationUI* evaluationUIPtr = new EvaluationUI(systemProductDB);
+				evaluationUIPtr->evaluateProduct(in_fp, out_fp);
+				break;
+			}
+			}
+			break;
+		}
+		case 6:
+		{
+			switch (menu_level_2)
+			{
+			case 1: // "6.1. 종료“ 메뉴 부분
+			{
+				program_exit(out_fp);
+				is_program_exit = 1;
+				break;
+			}
+			}
+			break;
+		}
+		}
+	}
+	return;
+}
+
+void program_exit(FILE* out_fp)
+{
+	fprintf(out_fp, "6.1. 종료\n");
+}
